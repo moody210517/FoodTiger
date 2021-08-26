ce6a8cc84f4e0275d80f0b3f7b9961a6e0536f59
carlgrob517 <carl517@outlook.com>
2021-08-26 21:26:29 +0800

    first commit

---
+++ bootstrap/cache/paddle-subscribe_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\PaddleSubscribe\\Providers\\Main',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\PaddleSubscribe\\Providers\\Main',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ resources/views/layouts/navbars/menus/staff.blade.php
@@ -0,0 +1,15 @@
+<ul class="navbar-nav">
+    @if(config('app.ordering'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('home') }}">
+                <i class="ni ni-tv-2 text-primary"></i> {{ __('POS') }}
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('orders.index') }}">
+                <i class="ni ni-basket text-orangse"></i> {{ __('Orders') }}
+            </a>
+        </li>
+    @endif
+
+</ul>
